7a36e65980ada8a7beb87d1285c0838e60867a5c
alirajuansills <alisill@mail.regent.edu>
2020-09-30 07:32:04 -0400

    organized files

---
+++ js-fundamentals/app.js
@@ -0,0 +1,37 @@
+// // var, let, const
+
+// var name = "John Doe";
+
+// name = "Steve Smith";
+
+// // Init var
+// var greeting;
+
+// // letters, numbers, _, $
+// // can not start with number
+
+// // multi word vars
+// var firstName = "John"; // Camel case
+// var first_name = "Sara"; // Underscore
+// var FirstName = "Tom"; // Pascal case
+
+// // LET
+// let name = "John Doe";
+// name = "John Doe";
+
+// // Const
+// // Can't be reassinged
+//  const name = "John";
+
+const person = {
+  name: "John",
+  age: 30,
+};
+
+person.name = "Sara";
+person.age = 32;
+
+const numbers = [1, 2, 3, 4, 5];
+numbers.push(6);
+
+// you can change arrays and objects that are assign to variables using const
