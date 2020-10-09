b8eaa440f082859f985241d0bd33afdeb0c8c972
alirajuansills <alisill@mail.regent.edu>
2020-10-09 08:11:47 -0400

    chapter 10

--- js-fundamentals/app.js
+++ js-fundamentals/app.js
@@ -108,32 +108,107 @@ Dynamically Typed Language
 
 /* Chapter 9: Type Conversion */
 
+// let val;
+
+// // Number to string
+// val = String(5);
+// val = String(4 + 4);
+// // Bool to string
+// val = String(true);
+// // Date to string
+// val = String(new Date());
+// // Array to string
+// val = String([1, 2, 3, 4]);
+// // toString()
+// val = (5).toString();
+// val = true.toString();
+// // String to number
+// val = Number("5");
+// val = Number(true);
+// val = Number(false);
+// val = Number(null);
+// val = Number("hello");
+// val = Number([1, 2, 3]);
+
+// val = parseInt("100.30");
+// val = parseFloat("100.30");
+// // Output
+// console.log(val);
+// console.log(typeof val);
+// // console.log(val.length);
+// console.log(val.toFixed());
+
+/* Chapter 10 Numbers & The Math Object */
+
+// const num1 = 100;
+// const num2 = 50;
+// let val;
+
+// // Simple math with numbers
+
+// val = num1 + num2;
+// val = num1 * num2;
+// val = num1 - num2;
+// val = num1 / num2;
+// val = num1 % num2;
+
+// // Math object
+// val = Math.PI;
+// val = Math.E;
+// val = Math.round(2.8);
+// val = Math.ceil(2.4);
+// val = Math.floor(2.8);
+// val = Math.sqrt(64);
+// val = Math.abs(-3);
+// val = Math.pow(8, 2);
+// val = Math.min(2, 33, 4, 1, 55, 6, 3, -2);
+// val = Math.max(2, 33, 4, 1, 55, 6, 3, -2);
+// val = Math.random();
+// val = Math.floor(Math.random() * 20 + 1); // commonly used
+
+// console.log(val);
+
+/* Chapter 11 String Methods & Concatenation */
+
+const firstName = "William";
+const lastName = "Johnson";
+const age = 27;
+
 let val;
 
-// Number to string
-val = String(5);
-val = String(4 + 4);
-// Bool to string
-val = String(true);
-// Date to string
-val = String(new Date());
-// Array to string
-val = String([1, 2, 3, 4]);
-// toString()
-val = (5).toString();
-val = true.toString();
-// String to number
-val = Number("5");
-val = Number(true);
-val = Number(false);
-val = Number(null);
-val = Number("hello");
-val = Number([1, 2, 3]);
+val = firstName + lastName;
 
-val = parseInt("100.30");
-val = parseFloat("100.30");
-// Output
+// Concatenation
+val = firstName + " " + lastName;
+
+// Append
+val = "Ali ";
+val += "Sills";
+
+val = "Hello, my name is" + firstName + " and I am " + age;
+
+// Escaping
+val = "that's awesome, I can't wait";
+
+// Length
+val = firstName.length;
+
+// concat
+val = firstName.concat(" ", lastName);
+
+// Change case
+val = firstName.toUpperCase();
+val = firstName.toLowerCase();
+
+val = firstName[2];
+
+// indexOf()
+val = firstName.indexOf("l");
+val = firstName.lastIndexOf("l");
+
+// charAt()
+val = firstName.charAt("2");
+// Get last char
+val = firstName.charAt(firstName);
+
 console.log(val);
-console.log(typeof val);
-// console.log(val.length);
-console.log(val.toFixed());
